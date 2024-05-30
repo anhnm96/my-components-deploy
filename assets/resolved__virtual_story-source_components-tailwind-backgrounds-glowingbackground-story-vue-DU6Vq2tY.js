1d7b65009cadb4b1bf56f3315e9255f284c2e11a
Nguyen Minh Anh <xiaosasori@gmail.com>
2024-05-30 19:06:16 +0700

    Updates

---
+++ assets/resolved__virtual_story-source_components-tailwind-backgrounds-glowingbackground-story-vue-DU6Vq2tY.js
@@ -0,0 +1,19 @@
+const resolved__virtual_storySource_componentsTailwindBackgroundsGlowingbackgroundStoryVue = `<script lang="ts" setup>
+import GlowingBackground from './GlowingBackground.vue'
+import GlowingDark from './GlowingDark.vue'
+<\/script>
+
+<template>
+  <Story title="Glowing Background">
+    <Variant title="Titl">
+      <GlowingBackground />
+    </Variant>
+    <Variant title="Dark">
+      <GlowingDark />
+    </Variant>
+  </Story>
+</template>
+`;
+export {
+  resolved__virtual_storySource_componentsTailwindBackgroundsGlowingbackgroundStoryVue as default
+};
